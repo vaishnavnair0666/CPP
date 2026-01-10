ec5ba4e4ed0cd70d39ad40192b1ef8f7a79e4e89
vaishnavnair0666 <vaishnavnair0666@gmail.com>
2026-01-10 15:53:16 +0530

    array reversal 2 ways

--- main.cpp
+++ main.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
-void swap(int &a, int &b) {
-  int temp{};
-  temp = b;
-  b = a;
-  a = temp;
+void print(const int *arr, int size) {
+  for (int i{0}; i < size; ++i) {
+    std::cout << arr[i] << " ";
+  }
+}
+void revswap(int *arr, int size) {
+  for (int i{0}; i < size / 2; ++i) {
+    arr[i] = arr[i] + arr[size - 1 - i];
+    arr[size - 1 - i] = arr[i] - arr[size - 1 - i];
+    arr[i] = arr[i] - arr[size - 1 - i];
+  }
+}
+void rev(const int *arr, int size, int *b) {
+  for (int i{size - 1}; i > -1; --i) {
+    b[i] = arr[size - 1 - i];
+  }
 }
 int main() {
 
-  int a{}, b{};
-  std::cin >> a >> b;
-  swap(a, b);
-  std::cout << "a:" << a << "b:" << b << "\n";
+  int a[4] = {1, 2, 3, 4};
+  int b[4];
+  std::cout << "arr a = ";
+  print(a, 4);
+  std::cout << "\narr b which is rev a = ";
+  rev(a, 4, b);
+  print(b, 4);
+  revswap(a, 4);
+  std::cout << "\narr a which is rev w/o b = ";
+  print(a, 4);
+  std::cout << "\n";
   return 0;
 }
