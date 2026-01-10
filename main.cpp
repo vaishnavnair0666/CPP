4aeeb79cbefdb895c50319660b12e35950a277a2
vaishnavnair0666 <vaishnavnair0666@gmail.com>
2026-01-11 05:15:08 +0530

    vectors and auto and itterators starting with algorithm

--- main.cpp
+++ main.cpp
@@ -1,25 +1,22 @@
-#include <cstddef>
+#include <algorithm>
 #include <iostream>
-#include <string>
-
-void print(const std::string &s) {
-  for (std::size_t i{0}; i < s.size(); ++i) {
-    std::cout << s.at(s.size() - 1 - i) << " ";
-  }
-  std::cout << "\n";
-}
+#include <vector>
 
 int main() {
-  std::string s;
+  std::vector<int> v{1, 2, 3, 4, 3, 5};
 
-  std::cout << "Enter a line: ";
-  std::getline(std::cin, s);
+  std::sort(v.begin(), v.end());
 
-  std::cout << "You entered: " << s << "\n";
-  std::cout << "Length: " << s.size() << "\n";
+  v.erase(std::remove(v.begin(), v.end(), 3), v.end());
+  std::cout << "after erase";
+  for (const auto &x : v) {
+    std::cout << x << " ";
+  }
 
-  s += "!";
-  print(s);
+  auto it = std::find(v.begin(), v.end(), 4);
+  if (it != v.end()) {
+    std::cout << "\nFound " << *it << "\n";
+  }
 
   return 0;
 }
