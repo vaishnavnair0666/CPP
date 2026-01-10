3d2e4093dd415c6c0e37126ab9d25af8c4a9329d
vaishnavnair0666 <vaishnavnair0666@gmail.com>
2026-01-10 16:01:53 +0530

    using a utility
    using size_t to infer positive values for size (always)

--- main.cpp
+++ main.cpp
@@ -1,33 +1,39 @@
 #include <iostream>
-void print(const int *arr, int size) {
-  for (int i{0}; i < size; ++i) {
+#include <utility>
+
+void print(const int *arr, size_t size) {
+  for (size_t i{0}; i < size; ++i) {
     std::cout << arr[i] << " ";
   }
 }
-void revswap(int *arr, int size) {
-  for (int i{0}; i < size / 2; ++i) {
-    arr[i] = arr[i] + arr[size - 1 - i];
-    arr[size - 1 - i] = arr[i] - arr[size - 1 - i];
-    arr[i] = arr[i] - arr[size - 1 - i];
-  }
-}
-void rev(const int *arr, int size, int *b) {
-  for (int i{size - 1}; i > -1; --i) {
+
+void rev(const int *arr, size_t size, int *b) {
+  for (size_t i{0}; i < size; ++i) {
     b[i] = arr[size - 1 - i];
   }
 }
-int main() {
 
+void revswap(int *arr, size_t size) {
+  for (size_t i{0}; i < size / 2; ++i) {
+    std::swap(arr[i], arr[size - 1 - i]);
+  }
+}
+
+int main() {
   int a[4] = {1, 2, 3, 4};
   int b[4];
+
   std::cout << "arr a = ";
   print(a, 4);
+
   std::cout << "\narr b which is rev a = ";
   rev(a, 4, b);
   print(b, 4);
+
   revswap(a, 4);
   std::cout << "\narr a which is rev w/o b = ";
   print(a, 4);
+
   std::cout << "\n";
   return 0;
 }
