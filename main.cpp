dc942e169b6735e6600dffba9f587d451d6e20d9
vaishnavnair0666 <vaishnavnair0666@gmail.com>
2026-01-10 11:48:12 +0530

    odd or even

--- main.cpp
+++ main.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
-using namespace std;
 
-int main() { cout << "Hello\n"; }
+int main() {
+
+  int x{};
+  std::cin >> x;
+  if (x % 2 == 0) {
+    std::cout << "even\n";
+  } else {
+    std::cout << "odd\n";
+  }
+  return 0;
+}
